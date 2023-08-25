44cf1fa8d8b6b405dbcb028f48ec535e63e1939f
Maysonbenoit <mib0195720@lpssonline.com>
2023-08-25 15:18:06 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -37,8 +37,12 @@ $(document).ready(function () {
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
 
-  // TODO 1: create a new shape object and add it to the array
-  
+  // TODO 1: create a new shape object and add it to the array 
+  var shape = {
+    color : "blue", 
+    shape : "circle",
+    repeat : 3
+  } ; dataShapes.push (shape)
 
   // TODO 2: add a new property to all data shapes
   
